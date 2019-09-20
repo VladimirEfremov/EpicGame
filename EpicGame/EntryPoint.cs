466bdb827809fa1909880ae56dce24da2e53426d
vezone <vezone@yandex.com>
2019-09-20 13:58:49 +0400

    Add functionality to UserDBHelper[1/2]

--- EpicGame/EntryPoint.cs
+++ EpicGame/EntryPoint.cs
@@ -15,7 +15,63 @@ namespace EpicGame
         static void Main(string[] args)
         {
             Console.WriteLine("It works!");
-            new UserDBHelper();
+
+            using (var db = new UserDBHelper())
+            {
+                while (true)
+                {
+                    var list = db.GetListOfUsers();
+                    foreach (var el in list)
+                    {
+                        Console.WriteLine($"{el.Id} {el.FullName} {el.Nickname} {el.Email}");
+                    }
+
+                    Console.WriteLine("1. Add user\n2. Remove user by id");
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.D1:
+                            {
+                                Console.WriteLine();
+                                Console.Write("FirstName: ");
+                                var fist = Console.ReadLine();
+                                Console.Write("SecondName: ");
+                                var sec = Console.ReadLine();
+                                Console.Write("Nickname: ");
+                                var nick = Console.ReadLine();
+                                Console.Write("Password: ");
+                                var pass = Console.ReadLine();
+                                Console.Write("Email: ");
+                                var email = Console.ReadLine();
+
+                                db.RegisterUserToTable(fist, sec, pass, nick, email);
+                                break;
+                            }
+                        case ConsoleKey.D2:
+                            {
+                                Console.WriteLine();
+                                System.Int32 id;
+                                Console.Write("Write id to delete: ");
+                                var idStr = Console.ReadLine();
+                                if (Int32.TryParse(idStr, out id))
+                                {
+                                    db.RemoveUserByIdFromTable(id);
+                                }
+                                break;
+                            }
+                        case ConsoleKey.D3:
+                            {
+
+                                break;
+                            }
+                        default: return;
+                    }
+
+                    
+
+                }
+            }
+
+
             Console.ReadKey();
         }
     }
--- EpicGame/src/DBHelper/UserDBHelper.cs
+++ EpicGame/src/DBHelper/UserDBHelper.cs
@@ -3,31 +3,102 @@
     using System.Linq;
     using EpicGame.src.Models;
 
-    class UserDBHelper 
+    class UserDBHelper : System.IDisposable
     {
+        private readonly UserTableEntity m_UserContext = new UserTableEntity();
+        private readonly UserRelationTableEntity m_UserRelationContext = new UserRelationTableEntity();
         public UserDBHelper()
         {
-            using (var context = new UserTableEntity())
+        }
+
+        public void Dispose()
+        {
+            m_UserContext.Dispose();
+            m_UserRelationContext.Dispose();
+        }
+
+        public void RegisterUserToTable(UserTable user)
+        {
+            var userIndex = FindUserByEmail(user.Nickname);
+            if (userIndex == -1)
             {
-                var list = context.UserTable.ToList();
-                //var user = context.UserTable.Find(1);
-                //if (user != null) context.UserTable.Remove(user);
-                context.UserTable.Add(new UserTable()
-                {
-                    FirstName = "Vova",
-                    SecondName = "Efremov",
-                    FullName = "Vova Efremov",
-                    PasswordHash = "123",
-                    Nickname = "vez",
-                    Email = "VE@yandex.com"
-                });
-                context.SaveChanges();
-                foreach (var el in list)
-                {
-                    System.Console.WriteLine($"{el.Id} {el.FirstName} {el.SecondName} {el.FullName} {el.Nickname} {el.Email}");
-                }
+                m_UserContext.UserTable.Add(user);
+            }
+            else
+            {
+                //Log.Warn: $"This user's nickname already exists in db! [id = {index + 1}]" 
             }
         }
+
+        public void RegisterUserToTable(string firstName, string secondName,
+            string passswordHash, string nickname, string email)
+        {
+            var userIndex = FindUserByEmail(email);
+            if (userIndex == -1)
+            {
+                m_UserContext.UserTable.Add(new UserTable()
+                {
+                    FirstName = firstName,
+                    SecondName = secondName,
+                    FullName = $"{firstName} {secondName}",
+                    PasswordHash = passswordHash,
+                    Nickname = nickname,
+                    Email = email
+                });
+                m_UserContext.SaveChanges();
+            }
+            else
+            {
+                //Log.Warn: $"This user's nickname already exists in db! [id = {index + 1}]" 
+            }
+        }
+
+        public bool IsRegisterUser(string email, string passwordHash)
+        {
+            var array = m_UserContext.UserTable.ToArray();
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i].Email.Equals(email) && array[i].PasswordHash.Equals(passwordHash))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void RemoveUserByIdFromTable(System.Int32 id)
+        {
+            var user = m_UserContext.UserTable.Find(id);
+            if (user != null)
+            {
+                m_UserContext.UserTable.Remove(user);
+                m_UserContext.SaveChanges();
+            }
+        }
+
+        [System.Runtime.CompilerServices.MethodImpl(256)]
+        private System.Int32 FindUserByEmail(string email)
+        {
+            var array = m_UserContext.UserTable.ToArray();
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i].Email.Equals(email))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public System.Collections.Generic.List<UserTable> GetListOfUsers()
+        {
+            return m_UserContext.UserTable.ToList();
+        }
+
+        //AddUserToFriends(User this, User toAdd)
+        //or
+        //AddUserToRelationGroup(User this, User toAdd, RelationGroup group)
+
     }
 
 
